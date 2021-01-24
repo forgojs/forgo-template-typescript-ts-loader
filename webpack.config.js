61a037c6096a096bd1594dfbadc62951f559bdee
Jeswin <jeswinpk@agilehead.com>
2021-01-24 07:41:22 +0530

    Switch to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.tsx',
@@ -18,5 +19,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist')
   },
 };
