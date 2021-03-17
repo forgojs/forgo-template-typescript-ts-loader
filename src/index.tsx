31bb45c10d91b5f0dc2077da3ad339989e98825d
Jeswin <jeswinpk@agilehead.com>
2021-03-18 03:51:26 +0530

    Update code

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ function Parent(props: ParentProps) {
         <div>
           <h1>Forgo App</h1>
           <p>
-            Welcome to Forgo. You'll need to edit this file in your favorite
+            Welcome to Forgo. Start by editing src/index.tsx in your favorite
             editor.
           </p>
           <p>Let's greet a couple of strangers.</p>
