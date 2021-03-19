818332ac657de0f6b0f4bc6550fea90b93448ce7
Jeswin <jeswinpk@agilehead.com>
2021-03-20 03:09:55 +0530

    Update webpack config. Omit index.js from index.html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  entry: "./src/index.tsx",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html")
-    })
+      template: path.resolve(__dirname, "index.html"),
+    }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, './dist')
+    contentBase: path.resolve(__dirname, "./dist"),
+    historyApiFallback: {
+      index: "/",
+    },
   },
 };
